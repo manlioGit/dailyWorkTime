170f1fbbefeebc671a3b9f3198ee77187fa29bdd
Manlio <manlio.modugno@gmtech.ch>
2015-05-08 15:55:03 +0200

    js refactor: callback function + object definition

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
 	  
 	var holidays = ['20150310', '20150319', '20150408'];
 
+	var eventData = {};
+	var calendarHeader = {left: 'prev today', center: 'title', right: 'next'};
+	
 	$('#timeForm').on('shown.bs.modal', function () {
   		$('#timeIn').focus();
 	});
@@ -16,16 +19,44 @@ $(document).ready(function() {
 		$('#eventForm').modal('show');
 	});
 
+	function dayRender(date, cell){
+		
+		var classValue = $(cell).attr('class');
+		if(classValue.contains("fc-sat") || classValue.contains("fc-sun")){
+			$(cell).css("background-color", "#f8f8f8");
+		}
+	
+		if($.inArray(date.format('YYYYMMDD'), holidays) >= 0){
+			$(cell).css("background-color", "#f8f8f8");
+			$(cell).text("Easter");
+		}
+	}
+	
+	function select(start, end) {
+		
+		if(start.isSame(end.clone().subtract(1,'days'))){
+			$("#timeForm").modal('show');
+			$("#toTimeFormBtn").show();
+		} 
+		else {
+			$("#eventForm").modal('show');
+			$("#toTimeFormBtn").hide();
+		}
+		
+		eventData.start = start;
+		eventData.end = end;
+//		$("#start").val(start);
+//		$("#end").val(end);
+	}
+	
 	$("#submitBtn").on('click', function() {
 
 		var title = $("option:selected").text();
 		if (title) {
-			var eventData = {
-				title: title,
-				start: moment($("#start").val()),
-				end: moment($("#end").val()),
-				color: "green"
-			};
+			
+			eventData.title = title;
+			eventData.color = "green";
+
 			$('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
 		}
 		$('#calendar').fullCalendar('unselect');
@@ -33,44 +64,18 @@ $(document).ready(function() {
 		$('#eventForm').modal('hide');
 		$('#timeForm').modal('hide');
 	});
-
-	$("#calendar").fullCalendar({
-		header: {
-			left: 'prev today',
-			center: 'title',
-			right: 'next'
-		},
-		firstDay: 1,
-		fixedWeekCount: false,
-		dayRender: function (date, cell) {
-			var classValue = $(cell).attr('class');
-			if(classValue.contains("fc-sat") || classValue.contains("fc-sun")){
-				$(cell).css("background-color", "#f8f8f8");
-			}
-		
-			if($.inArray(date.format('YYYYMMDD'), holidays) >= 0){
-				$(cell).css("background-color", "#f8f8f8");
-				$(cell).text("Easter");
-			}
-    		},
-	    	selectable: true,
-		select: function(start, end) {
-			var compare = end.clone();
-			if(start.isSame(compare.subtract(1,'days'))){
-				$("#timeForm").modal('show');
-				$("#toTimeFormBtn").show();
-				$("#start").val(start);
-				$("#end").val(end);
-			
-			} 
-			else {
-				$("#eventForm").modal('show');
-				$("#toTimeFormBtn").hide();
-				$("#start").val(start);
-				$("#end").val(end);
-			}
-		},
-		editable: true,
-		eventLimit: true
-	});
+	
+	
+	var fullCalendar = {
+			header: calendarHeader,
+			firstDay: 1,
+			fixedWeekCount: false,
+			dayRender: dayRender,
+	    	select: select,
+		    selectable: true,
+			editable: true,
+			eventLimit: true
+		}
+	
+	$("#calendar").fullCalendar(fullCalendar);
 });
